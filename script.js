b6b1c65654881ed075b201d9f67a79ebfd495df8
guiver <guiver95@gmail.com>
2019-11-03 17:30:11 -0300

    se agrega funcion para calcular  edad

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(function() {
-    let idsaludo = '#saludo';
-    $(idsaludo).text(''); //delete the previous text
-    
+    const fechaHoy = new Date();
+    const fechaNacimiento = new Date(1995, 9, 30);
+    const fechaPrimeraExperienciaLaboral = new Date(2018, 0, 1);
+
     function write(elementId, msg, phrase, index) {
       if (index == msg.length) return;
     
@@ -12,11 +13,26 @@ $(function() {
         write(elementId, msg, phrase, ++index);
       }, 120);
     }
-    
-    write(idsaludo, "Hi, I'm Guille", '', 0);
-    //write(idSaludo2, "Hola, soy Guille", '', 0);
 
+    const aniosEntreFechas = (fechaMayor, fechaMenor) => {
+      const diferencia = fechaMayor.getTime() - fechaMenor.getTime();
+      return Math.floor(diferencia / (1000 * 60 * 60 * 24 * 365));
+    }
+
+    // efecto de escritura en el saludo
+    $('#saludo').text(''); 
+    write('#saludo', "Hi, I'm Guille", '', 0);
+    
+    // efecto subtitulo
     $("#subtitulo").hide();
     $("#subtitulo").slideDown( "slow" );
+
+    // calcula y muestra la edad actual
+    const edadActual = aniosEntreFechas(fechaHoy, fechaNacimiento);
+    $('#edad').text(edadActual);
+
+    // calcula y muestra anios experiencia laboral
+    const aniosExperiencia = aniosEntreFechas(fechaHoy, fechaPrimeraExperienciaLaboral);
+    $('#experiencia').text(aniosExperiencia);
     
   });
